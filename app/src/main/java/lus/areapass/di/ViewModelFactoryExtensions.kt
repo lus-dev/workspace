9d4f6e92a5fdce1ef4e5c5e5e8686018e0b9320f
lus <alexander.lysenko.dev@gmail.com>
2020-10-22 17:22:31 +0300

    Provide Hilt DI basic implementation

--- app/src/main/java/lus/areapass/di/ViewModelFactoryExtensions.kt
+++ app/src/main/java/lus/areapass/di/ViewModelFactoryExtensions.kt
@@ -5,10 +5,25 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 
-inline fun <reified T : ViewModel> FragmentActivity.viewModel(crossinline initializer: (T) -> Unit) =
-    viewModels<T>().apply { initializer(this.value) }
+inline fun <reified T : ViewModel> FragmentActivity.viewModel(crossinline initialBlock: (T) -> Unit) =
+    viewModels<T> {
+        object : ViewModelProvider.Factory {
+            override fun <F : ViewModel> create(modelClass: Class<F>): F {
+                return ViewModelProvider(this@viewModel).get(modelClass)
+                    .apply { initialBlock(this as T) }
+            }
+        }
+    }
 
-inline fun <reified T : ViewModel> Fragment.viewModel(crossinline initializer: (T) -> Unit) =
-    viewModels<T>().apply { initializer(this.value) }
+inline fun <reified T : ViewModel> Fragment.viewModel(crossinline initialBlock: (T) -> Unit) =
+    viewModels<T> {
+        object : ViewModelProvider.Factory {
+            override fun <F : ViewModel> create(modelClass: Class<F>): F {
+                return ViewModelProvider(this@viewModel).get(modelClass)
+                    .apply { initialBlock(this as T) }
+            }
+        }
+    }
